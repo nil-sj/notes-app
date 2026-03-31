8761e2282289f31aacf743ae479de9147593de20
Niladri Jyoti <niladrijyoti@gmail.com>
2026-03-31 08:49:53 -0400

    Add missing code for DB Connection

--- index.js
+++ index.js
@@ -59,4 +59,22 @@ app.use((err, req, res, next) => {
 })
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+async function startServer() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000,
+    })
+
+    console.log('MongoDB connected')
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`)
+    })
+  } catch (err) {
+    console.error('MongoDB connection error:', err)
+    process.exit(1)
+  }
+}
+
+startServer()
